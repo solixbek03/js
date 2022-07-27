538365273c02c3298ebe869378bc1e2fc6fb929f
solixbek03 <clashbommm@gmail.com>
2022-07-27 09:56:20 +0500

    add js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var header_alert = alert("Welcome");
+var first_name = prompt("First name", "solihbek");
+var last_name = prompt("Last name", "abdullabekov");
+var Age = prompt("years old", "20");
+var occupation = prompt("occupation", "policeman");
+var address = prompt("address", "shamskulol street");
+var thanks = confirm("thank you very much");
+
+console.log(`First name: ${first_name} ${last_name}`);
+console.log(`User date: ${Age}`);
+console.log(`Occupation: ${occupation}`);
+console.log(`address: ${address}`);
